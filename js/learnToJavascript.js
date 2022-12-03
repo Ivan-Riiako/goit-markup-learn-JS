d19aebd7b233f7f0cec84a4a48b8273fd4d950a5
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2022-12-03 18:24:29 +0200

    4

--- js/learnToJavascript.js
+++ js/learnToJavascript.js
@@ -1,7 +1,7 @@
 // 'use strict'; // не нужем когда при подключении скрипта пишем type="module"
 let age = 1 / 2;
 const message = `'use strict' - не нужен когда при подключении скрипта пишем type="module"`;
-console.log(`sum letters of (${message})=  ${message.length}`); 
+console.log(`sum letters of (${message})=  ${message.length}`);
 // box with colors
 const colors = [
   'tomato',
@@ -40,34 +40,66 @@ console.log(`Namber of colors : ${index - 1}`);
 
 document.querySelector('.hero-section').style.backgroundColor = colorSection;
 
+// задача с курьером
+// let option = 0;
+// let alertMessage = '';
 
-// задача с курьером 
-let option = 0;
-let alertMessage = '';
+// switch (option) {
+//   case 1:
+//     alertMessage = 'Вы можете забрать товар завтра с 12 в нашем офисе!';
+//     console.log(alertMessage);
+//     break;
 
-switch (option) {
-  case 1:
-    alertMessage = 'Вы можете забрать товар завтра с 12 в нашем офисе!';
-    console.log(alertMessage);
-    break;
-  
-  case 2:
-    alertMessage = 'Курьер доставит заказ завтра с 9-00 до 18-00';
-    console.log(alertMessage);
-    break;
-  
-  case 3:
-    alertMessage = 'Посылка будет отправоенна сеголня!';
-    console.log(alertMessage);
-    break;
-  default: 
-    alertMessage = 'Вам перзвонит менеджер';
+//   case 2:
+//     alertMessage = 'Курьер доставит заказ завтра с 9-00 до 18-00';
+//     console.log(alertMessage);
+//     break;
+
+//   case 3:
+//     alertMessage = 'Посылка будет отправоенна сеголня!';
+//     console.log(alertMessage);
+//     break;
+//   default:
+//     alertMessage = 'Вам перзвонит менеджер';
+// }
+// console.log(alertMessage);
+
+// loop
+// for (let i = 1; i < 30; i += 1){
+//   console.log(i);
+// }
+
+// задачас с зарплатами для работяг
+// const employees = 9;
+// const minSalary = 600;
+// const maxSalary = 3000;
+// console.log(`сотрудники: ${employees}  `);
+
+// let totalSalary = 0;
+
+// for (let i = 0; i < employees; i += 1){
+//   let randomSalary = Math.round(Math.random() * (maxSalary - minSalary) + minSalary)
+// console.log(`Зарплата рабочего № ${i+1} : ${randomSalary} shekel`);
+//   totalSalary += randomSalary;
+// }
+// console.log(`Сумма всех зарплат: ${totalSalary}  `);
+
+// посчет только чисел которые делятся на 2
+const minNumber = 10;
+const maxNumber = 30;
+let total = 0;
+
+for (let i = minNumber; i < maxNumber; i += 1) {
+
+  // Пропускаем если остаток от деления не 0
+
+  if (i % 2 !== 0) {
+    console.log(`Число № ${i - minNumber + 1}   не четное ${i}`);
+    continue;
+  }
+  console.log(`Число № ${i - minNumber + 1}  четное ${i}`);
+  // сумируем все четные цифры
+
+  total += i;
 }
-console.log(alertMessage);
-
-
-for (let i = 1; i < 30; i += 2){
-  console.log(i);
-  i += 0.6;
-  i = Number.parseInt(i);
-}
+console.log(`Sum of even numbers : ${total}`);
