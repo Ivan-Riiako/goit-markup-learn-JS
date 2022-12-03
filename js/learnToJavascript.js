52e706ee73b31eb284b1fa0a9ffed1b5fd4e8e42
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2022-12-03 12:40:54 +0200

    hero-section change bg-color

--- js/learnToJavascript.js
+++ js/learnToJavascript.js
@@ -1,2 +1,34 @@
-console.log("BFG");
-alert("Gornilo");
+'use strict';
+let age = 1 / 2;
+const message = "Welcome to Bhhhhhhhhhhhhhhhuuuuuuuuuuuuuuuuuuuuuuahamas!";
+console.log(message.length); // 19
+
+const colors = [
+  'tomato',
+  'teal',
+  'orange',
+  'skyblue',
+  'deeppink',
+  'Fuchsia',
+  'Olive',
+  'SlateGray',
+  'Crimson',
+  'YellowGreen',
+  'Purple',
+  'Lime',
+];
+const max = colors.length - 1;
+const min = 0;
+const index = Math.round(Math.random() * (max - min) + min);
+console.log(index);
+const color = colors[index];
+const colorSection = colors[index- 1];
+
+console.log(color);
+document.body.style.backgroundColor = color;
+
+// Change backgroundColor hero-section  
+document.querySelector('.hero-section').style.backgroundColor = colorSection;
+
+
+
