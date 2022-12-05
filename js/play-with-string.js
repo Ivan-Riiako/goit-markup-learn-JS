99bcedb36a6d0ad16385509298f11dd1b525fc68
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2022-12-05 23:51:31 +0200

    5

---
+++ js/play-with-string.js
@@ -0,0 +1,22 @@
+
+const fontSize = '15,1888px';
+let fontSizeOnlyNumbers;
+
+// // check spam
+// function checkForSpam(message) {
+//   let result;
+//     result =
+//     message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale');
+//   return result;
+// }
+// let maill = checkForSpam("feev  SAle");
+// console.log(maill)
+
+// // check for null and undefined 
+// const incomingValue = undefined;// null; 
+// const defaultValue = 404;// all others
+// let value = incomingValue ?? defaultValue;
+// console.log(value);// 404 , if incomingValue = all others; value = all others 
+
+
+
