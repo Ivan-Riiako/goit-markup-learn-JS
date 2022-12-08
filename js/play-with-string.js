fe717b971167667fbfc8d958fc3749a28cb3c854
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2022-12-08 16:28:31 +0200

    ! - Inverse

--- js/play-with-string.js
+++ js/play-with-string.js
@@ -14,7 +14,7 @@
 // // const bmi = parseFloat(weight) / (parseFloat(height**2));
 
 // console.log(
-// `weight:${weight} 
+// `weight:${weight}
 // height:${height}`);
 // console.log(`${bmi.toFixed(1)}`)
 
@@ -32,22 +32,34 @@
 
 
 
-// // check for null and undefined 
-// const incomingValue = undefined;// null; 
+// // check for null and undefined
+// const incomingValue = undefined;// null;
 // const defaultValue = 404;// all others
 // let value = incomingValue ?? defaultValue;
-// console.log(value);// 404 , if incomingValue = all others; value = all others 
+// console.log(value);// 404 , if incomingValue = all others; value = all others
+
+// // clock
+// const totalMinute = 1440;// 24 hours
+// let hours =( totalMinute / 60).toFixed(0);
+// let minute = totalMinute % 60;
+// if (minute < 10) {
+//   minute= `0${minute}`
+// }
+// if (hours < 10) {
+//  hours= `0${hours}`;
+// }
+// const clock = `${hours}:${minute}`;
+
+// console.log(clock);
 
 
-const totalMinute = 1440;// 24 hours
-let hours =( totalMinute / 60).toFixed(0);
-let minute = totalMinute % 60;
-if (minute < 10) {
-  minute= `0${minute}`  
+// add "/"
+let link = "https://ivan-riiako.github.io/goit-markup-learn-JS";
+
+if (!link.endsWith('/')) {     //  ! save time
+   console.log(link + '/');
+    
+} else {
+   console.log(link);
+    
 }
-if (hours < 10) {
- hours= `0${hours}`;
-}
-const clock = `${hours}:${minute}`;
-
-console.log(clock);
