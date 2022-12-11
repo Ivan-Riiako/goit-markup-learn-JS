ad195515c2d230a1c90a6956a1fc141aee954d0b
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2022-12-11 18:35:49 +0200

    5

--- js/module-2_arrayAndFunction.js
+++ js/module-2_arrayAndFunction.js
@@ -1,1 +1,7 @@
-const arr
+const friends = ['frien1', 'frien-2', 'frien3', 'frien4', 'frien5', 'frien6', 'frien7', 'frien8', 'frien9'];
+console.table(friends);
+const lastIndexFriends = friends.length - 1;
+console.log(lastIndexFriends);
+for (let i = 0; i <= lastIndexFriends; i += 1){
+    console.log(friends[i])
+}
