2ad015dab4bd77969e42eaf4b33daa90753911ab
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2022-12-11 20:04:19 +0200

    5

--- js/module-2_arrayAndFunction.js
+++ js/module-2_arrayAndFunction.js
@@ -1,10 +1,41 @@
-const friends = ['friend1', 'friend2', 'friend3', 'friend4', 'friend5', 'friend6', 'friend7', 'friend8', 'friend9'];
-console.table(friends);
+// const friends = ['friend', 'friend', 'friend', 'friend', 'friend', 'friend', 'friend', 'friend', 'friend'];
+// console.table(friends);
 
-const lastIndexFriends = friends.length - 1;
-console.log(lastIndexFriends);
+// const lastIndexFriends = friends.length - 1;
+// console.log(lastIndexFriends);
 
-for (let i = 0; i <= lastIndexFriends; i += 1){
-    friends[i] += 33+33+"cook"+1+1*5;
-    console.log(friends[i])
+// for (let i = 0; i <= lastIndexFriends; i += 1){
+//     friends[i] += `-${i+1}`;
+//     console.log(friends[i]);
+// }
+// // without anaviable
+// const clients = [
+//   'client',
+//   'client',
+//   'client',
+// ];
+// for (const client of clients) {
+//     console.log(client);
+// }
+
+const cart = [32, 23, 46, 46, 46, 57, 65, 67, 87, 91, 21, 46, 17, 120];
+let totalCart = 0;
+
+// // var 1
+// for (let i = 0; i < cart.length; i += 1){
+//     totalCart += cart[i];
+// }
+
+// var 2
+for (const value of cart) {
+    totalCart += value;
 }
+console.log(`Total: ${totalCart}`);
+
+for (let i = 0; i <= cart.length; i += 1) {
+    cart[i] *= 1.1;
+   
+console.log(cart[i]);
+
+}
+console.log(cart);
