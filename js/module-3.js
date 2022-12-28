34e7f02e02afdbe890b3b24453e5f14e6b95d4d4
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2022-12-28 11:11:30 +0200

    Update module-3.js

--- js/module-3.js
+++ js/module-3.js
@@ -1,2 +1,32 @@
-const objeckt = { name: sveta, };
-console.log(objeckt.op);
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'На берегу спокойных вод',
+    author: 'Роберт Шекли',
+    rating: 8.51,
+  },
+  {
+    title: 'Сон смешного человека',
+    author: 'Федор Достоевский',
+    rating: 7.75,
+  },
+];
+
+for (const book of books) {
+  console.log(book);
+//   console.log(book.title);
+//   console.log(book.author);
+//   console.log(book.rating);
+}
+
+// const bookNames = [];
+
+// for (const book of books) {
+//   bookNames.push(book.title);
+// }
+
+// console.log(bookNames);
