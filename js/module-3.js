d6af87836f609338c8c62dee6fdc789124df61fe
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2022-12-28 18:23:27 +0200

    Update module-3.js

--- js/module-3.js
+++ js/module-3.js
@@ -1,32 +1,16 @@
-const books = [
-  {
-    title: 'The Last Kingdom',
-    author: 'Bernard Cornwell',
-    rating: 8.38,
-  },
-  {
-    title: 'На берегу спокойных вод',
-    author: 'Роберт Шекли',
-    rating: 8.51,
-  },
-  {
-    title: 'Сон смешного человека',
-    author: 'Федор Достоевский',
-    rating: 7.75,
-  },
-];
+const Transaction = {
+    DEPOSIT: 'deposit',
+    WITHDRAW: 'withdraw',
+};
 
-for (const book of books) {
-  console.log(book);
-//   console.log(book.title);
-//   console.log(book.author);
-//   console.log(book.rating);
+const account = {
+    balance: 0,
+    transaction: [],
+    createTransaction(amount, type) {
+        const transaction = {
+        amount,
+        type,
+        date=
+        };
+    }
 }
-
-// const bookNames = [];
-
-// for (const book of books) {
-//   bookNames.push(book.title);
-// }
-
-// console.log(bookNames);
