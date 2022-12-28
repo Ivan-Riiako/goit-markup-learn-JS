6e8bec62cb0798538e496ff4477006a0c161ca91
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2022-12-28 22:03:41 +0200

    Update module-3.js

--- js/module-3.js
+++ js/module-3.js
@@ -14,10 +14,10 @@ const Transaction = {
 
 const account = {
   balance: 0,
-    transactions: [
-        { amount: 10000, type: 'deposit', id: 1672249694281 },
-        { amount: 1000, type: 'withdraw', id: 1672249694487 }
-    ],
+  transactions: [
+    { amount: 1000, type: 'deposit', id: 1672249694281 },
+    { amount: 1000, type: 'withdraw', id: 1672249694487 },
+  ],
 
   createTransaction(amount, type) {
     const transaction = {
@@ -38,45 +38,49 @@ const account = {
   withdraw(amount) {
     if (this.balance < amount) {
       return console.log('Operation is not sisefull: cash is nan');
-    } 
-      const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
-      this.transactions.push(transaction);
-      this.balance -= amount;
-    
-    },
-  
-    getBalance() {
-      alert(this.balance)
-    },
-    
-    getTransitionDetails(id) {
-        for (const transaction of this.transactions) {
-            if (id === transaction.id) {
-                console.log(transaction);
-                return transaction;
-                
-        }
     }
-},
-  
-    getTransactionTotal(type) {
-        let total = 0;
-        for (const transaction of this.transactions) {
-            if (transaction.type === type) {
-                total += transaction.amount;
-            }
-        }
-        return total;
-    }
+    const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
+    this.transactions.push(transaction);
+    this.balance -= amount;
+  },
 
+  getBalance() {
+    alert(this.balance);
+  },
+
+  getTransitionDetails(id) {
+    for (const transaction of this.transactions) {
+      if (id === transaction.id) {
+        console.log(transaction);
+        return transaction;
+      }
+    }
+  },
+
+  getTransactionTotal(type) {
+    let total = 0;
+    for (const transaction of this.transactions) {
+      if (transaction.type === type) {
+        total += transaction.amount;
+      }
+    }
+    return total;
+  },
 };
 
-
-account.deposit(10000);
-// Flesh
-setTimeout(() => {
-account.withdraw(1000)
-},200)
 console.log(account);
-account.getTransitionDetails(1672249694281);
-console.log(account.getTransactionTotal(Transaction.WITHDRAW));
+account.deposit(10000);
+account.withdraw(2000);
+
+// // Flesh
+// setTimeout(() => {
+// account.withdraw(1000);
+// }, 200)
+
+console.log(account);
+// account.getTransitionDetails(1672249694281);
+// account.getTransitionDetails(1672249694487);
+
+
+// console.log(account.getTransactionTotal(Transaction.WITHDRAW));
+// console.log(account.getTransactionTotal(Transaction.DEPOSIT));
