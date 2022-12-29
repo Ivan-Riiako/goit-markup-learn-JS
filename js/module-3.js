7ed623d984b0fdc655e1a9dec6291602db9ca2b5
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2022-12-29 13:27:30 +0200

    5

--- js/module-3.js
+++ js/module-3.js
@@ -12,65 +12,65 @@ const Transaction = {
 
 // user.sayHello();
 
-const account = {
-  balance: 0,
-  transactions: [
-    { amount: 1000, type: 'deposit', id: 1672249694281 },
-    { amount: 1000, type: 'withdraw', id: 1672249694487 },
-  ],
+// const account = {
+//   balance: 0,
+//   transactions: [
+//     { amount: 1000, type: 'deposit', id: 1672249694281 },
+//     { amount: 1000, type: 'withdraw', id: 1672249694487 },
+//   ],
 
-  createTransaction(amount, type) {
-    const transaction = {
-      amount,
-      type,
-      id: Date.now(),
-    };
+//   createTransaction(amount, type) {
+//     const transaction = {
+//       amount,
+//       type,
+//       id: Date.now(),
+//     };
 
-    return transaction;
-  },
+//     return transaction;
+//   },
 
-  deposit(amount) {
-    const transaction = this.createTransaction(amount, Transaction.DEPOSIT);
-    this.transactions.push(transaction);
-    this.balance += amount;
-  },
+//   deposit(amount) {
+//     const transaction = this.createTransaction(amount, Transaction.DEPOSIT);
+//     this.transactions.push(transaction);
+//     this.balance += amount;
+//   },
 
-  withdraw(amount) {
-    if (this.balance < amount) {
-      return console.log('Operation is not sisefull: cash is nan');
-    }
-    const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
-    this.transactions.push(transaction);
-    this.balance -= amount;
-  },
+//   withdraw(amount) {
+//     if (this.balance < amount) {
+//       return console.log('Operation is not sisefull: cash is nan');
+//     }
+//     const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
+//     this.transactions.push(transaction);
+//     this.balance -= amount;
+//   },
 
-  getBalance() {
-    alert(this.balance);
-  },
+//   getBalance() {
+//     alert(this.balance);
+//   },
 
-  getTransitionDetails(id) {
-    for (const transaction of this.transactions) {
-      if (id === transaction.id) {
-        console.log(transaction);
-        return transaction;
-      }
-    }
-  },
+//   getTransitionDetails(id) {
+//     for (const transaction of this.transactions) {
+//       if (id === transaction.id) {
+//         console.log(transaction);
+//         return transaction;
+//       }
+//     }
+//   },
 
-  getTransactionTotal(type) {
-    let total = 0;
-    for (const transaction of this.transactions) {
-      if (transaction.type === type) {
-        total += transaction.amount;
-      }
-    }
-    return total;
-  },
-};
+//   getTransactionTotal(type) {
+//     let total = 0;
+//     for (const transaction of this.transactions) {
+//       if (transaction.type === type) {
+//         total += transaction.amount;
+//       }
+//     }
+//     return total;
+//   },
+// };
 
-console.log(account);
-account.deposit(10000);
-account.withdraw(2000);
+// console.log(account);
+// account.deposit(10000);
+// account.withdraw(2000);
 
 // // Flesh
 // setTimeout(() => {
