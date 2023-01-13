b25ed125c61a7dbd907275a36119d4ef3c36e6b0
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-01-13 18:04:37 +0200

    6

--- js/module-4.js
+++ js/module-4.js
@@ -15,34 +15,73 @@
 // repeat(8, prettyPrint);
 
 
-let i = 0;
+// let i = 0;
 
-function createProduct(partialProduct, callback) {
-    const object = {
-        id:Date.now(),
-    ...partialProduct
+// function createProduct(partialProduct, callback) {
+//     i += 1;
+//     const object = {
+//         id:i,
+//     ...partialProduct
+//     }
+//     callback(object);
+// };
+
+// createProduct({
+//     name: 'Strawberry',
+//     price: 105,
+//     quantity: 3,
+// }, logProduct);
+
+// function logProduct(product) {
+//     console.log(product);
+// }
+// function logTotalPrice({ price, quantity }) {
+//     console.log(price * quantity);
+//     return price * quantity;
+// }
+
+// createProduct(
+//   {
+//     name: 'Apple',
+//     price:58,
+//     quantity:8,
+//   },
+//   logTotalPrice
+// );
+
+
+
+let TRANSACTION_LIMIT = 1000;
+
+const account = {
+  username: 'Jacob',
+  balance: 3400,
+  withdraw(amount, onSuccess, onError) {
+    if (amount > this.balance) {
+      onError('a bit of $ on balance');
+    } else if (amount > TRANSACTION_LIMIT) {
+      onError('a bit of $ on TRANSACTION_LIMIT');
+    } else {
+      onSuccess('Operation Success');
     }
-    callback(object);
+  },
+  deposit(amount, onSuccess, onError) {
+    this.account += amount;
+    onSuccess(`Yes`);
+  },
 };
 
-createProduct({
-    name: 'Strawberry',
-    price: 105,
-    quantity: 3,
-}, logProduct);
 
-function logProduct(product) {
-    console.log(product);
-}
-function logTotalPrice(product) {
-    console.log(product);
+
+function hendleSuccess(message) {
+    console.log(`Success: ${message}`);
 }
 
-createProduct(
-  {
-    name: 'Vanilla',
-    price: 358,
-    quantity: 9,
-  },
-  logTotalPrice
-);
+function hendleError(message) {
+    console.log(`Error: ${message}`);
+};
+
+
+account.withdraw(300, hendleSuccess, hendleError);
+account.withdraw(500, hendleSuccess, hendleError);
+account.withdraw(1000, hendleSuccess, hendleError);
