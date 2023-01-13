28802eb3e4345bad3d313935c22dccbba68b9558
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-01-13 21:45:40 +0200

    Update module-4.js

--- js/module-4.js
+++ js/module-4.js
@@ -51,41 +51,62 @@
 
 
 
-let TRANSACTION_LIMIT = 1000;
+// let TRANSACTION_LIMIT = 1000;
 
-const account = {
-  username: 'Jacob',
-  balance: 3400,
-  withdraw(amount, onSuccess, onError) {
-    if (amount > this.balance) {
-      onError(`Невозможно снять :${amount}, на балансе ${this.balance}`);
-    } else if (amount > TRANSACTION_LIMIT) {
-      onError(`Сумаа снятия: ${amount}, больше чем лимит транзакций- ${TRANSACTION_LIMIT}`);
-    } else {
-        this.balance -= amount;
-        onSuccess(`Операция успешна.Сняли: ${ amount }. Осталось: ${this.balance}`);
-    }
-  },
-  deposit(amount, onSuccess, onError) {
-    this.account += amount;
-    onSuccess(`Yes`);
-  },
-};
-
-
-
-function hendleSuccess(message) {
-    console.log(`Success: ${message}`);
-}
-
-function hendleError(message) {
-    console.log(`Error: ${message}`);
-};
+// const account = {
+//   username: 'Jacob',
+//   balance: 3400,
+//   withdraw(amount, onSuccess, onError) {
+//     if (amount > this.balance) {
+//       onError(`Невозможно снять :${amount}, на балансе ${this.balance}`);
+//     } else if (amount > TRANSACTION_LIMIT) {
+//       onError(`Сумаа снятия: ${amount}, больше чем лимит транзакций- ${TRANSACTION_LIMIT}`);
+//     } else {
+//         this.balance -= amount;
+//         onSuccess(`Операция успешна.Сняли: ${amount}. На счету: ${this.balance}`);
+//     }
+//   },
+//   deposit(amount, onSuccess, onError) {
+      
+//       if (amount <= 0){
+//         onError(`Невозможно зачислить :${amount}`);
+//       } else if (amount > TRANSACTION_LIMIT) {
+//         onError(`Сумаа зачисления: ${amount}, больше чем лимит транзакций - ${TRANSACTION_LIMIT}`);
+//       } else {
+//         this.account += amount;
+//         onSuccess(`Операция успешна.Зачислено: ${amount}. На счету: ${this.balance}`);
+//       }
+//   },
+// };
 
 
-account.withdraw(300, hendleSuccess, hendleError);
-account.withdraw(900, hendleSuccess, hendleError);
-account.withdraw(3000, hendleSuccess, hendleError);
-account.withdraw(1300, hendleSuccess, hendleError);
-account.withdraw(302, hendleSuccess, hendleError);
-account.withdraw(199, hendleSuccess, hendleError);
+
+// function hendleSuccess(message) {
+//     console.log(`Success: ${message}`);
+// }
+
+// function hendleError(message) {
+//     console.log(`Error: ${message}`);
+// };
+
+
+// account.withdraw(300, hendleSuccess, hendleError);
+// account.withdraw(900, hendleSuccess, hendleError);
+// account.withdraw(3000, hendleSuccess, hendleError);
+// account.withdraw(1300, hendleSuccess, hendleError);
+// account.withdraw(302, hendleSuccess, hendleError);
+// account.withdraw(199, hendleSuccess, hendleError);
+
+// account.deposit(300, hendleSuccess, hendleError);
+// account.deposit(900, hendleSuccess, hendleError);
+// account.deposit(3000, hendleSuccess, hendleError);
+// account.deposit(1300, hendleSuccess, hendleError);
+// account.deposit(302, hendleSuccess, hendleError);
+// account.deposit(199, hendleSuccess, hendleError);
+
+
+// const numbers = [1, 2, 3, 4, 5];
+// const filteredNumbers = numbers.filter(value => value > 3);
+// console.log(filteredNumbers); // [4, 5]
+
+
